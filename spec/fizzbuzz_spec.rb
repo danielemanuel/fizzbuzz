07496c162952df5a93567cc3126f6b19d44c3ef9
danielemanuel <daniel.costea@vkernel.ro>
2017-04-09 21:12:54 +0100

    rspec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,21 +4,15 @@ describe "fizzbuzz" do
   it "returns 'fizz' for the  3" do
     expect(fizzbuzz(3)).to eq "fizz"
   end
-end
 
-describe "fizzbuzz" do
   it "returns 'buzz' when pass 5" do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-end
 
-describe "fizzbuzz" do
   it "returns 'Fizzbuzz' when pass 15" do
     expect(fizzbuzz(15)).to eq 'Fizzbuzz'
   end
-end
 
-describe "fizzbuzz" do
   it "returns 'fizz' when pass 11" do
     expect(fizzbuzz(11)).to eq 11
   end
